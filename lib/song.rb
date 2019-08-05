2e4a8eba72a3c8fed499d813008c6103547c120e
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-05 18:07:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,10 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
+  @@artists = []
   def initialize(name, artist, genre)
     @@genres << genre
+    @@artists << artist
     @@count += 1
   end
 
